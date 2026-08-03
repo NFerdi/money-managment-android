ffb7accdf963133d5d471d1ffbe5540a87ee850e
ferdi <ikahsoadah@gmail.com>
2026-08-03 20:42:57 +0700

    chore: setup expo router

---
+++ app/_layout.tsx
@@ -0,0 +1,15 @@
+import '../global.css'
+import "react-native-reanimated"
+import "react-native-gesture-handler"
+
+import {Stack} from 'expo-router'
+
+export default function RootLayout(){
+    return (
+        <Stack 
+            screenOptions={{
+                headerShown: false
+            }}
+        />
+    )
+}
---
+++ app/index.tsx
@@ -0,0 +1,11 @@
+import {Redirect} from 'expo-router'
+
+import React from 'react'
+
+export default function index() {
+    const isLogin = false
+
+    if(!isLogin) return <Redirect href="/(auth)/login" />
+
+    return <Redirect href="/(dashboard)/dashboard" />
+}
